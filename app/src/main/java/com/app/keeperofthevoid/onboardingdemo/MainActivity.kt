790b71a07187f8fce8d47c5daf4bae332fe7e4be
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2019-06-15 14:01:17 -0500

    Add Pre-Existing Project Files
    - Add pre-existing project files to fix some issues

--- app/src/main/java/com/app/keeperofthevoid/onboardingdemo/MainActivity.kt
+++ app/src/main/java/com/app/keeperofthevoid/onboardingdemo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.app.keeperofthevoid.onboardingdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mSlideViewPager: ViewPager
+    private lateinit var mDotLayout: LinearLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mSlideViewPager = findViewById(R.id.slideViewPager)
+        mDotLayout = findViewById(R.id.dotsLayout)
     }
 }
